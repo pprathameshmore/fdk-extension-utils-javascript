5e916a30f08a0c1961b2b0e4b2168197af052d94
gaurav kumar sharma <gauravkumarsharma@gofynd.com>
2022-07-14 16:36:06 +0530

    extension subscription handler added

---
+++ handlers/webhook-handler.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const { SubscriptionModel } = require('../models/mongoose/subscription');
+
+class WebhookHandler {
+    constructor(model) {
+        this.model = model;
+    }
+
+    handleExtensionInstall(eventName, payload, companyId, applicationId){
+        console.log(`Event ${eventName} received for ${companyId} and ${applicationId}`);
+        console.log(payload);
+    }
+    
+    handleExtensionUninstall(eventName, payload, companyId, applicationId){
+        console.log(`Event ${eventName} received for ${companyId} and ${applicationId}`);
+        console.log(payload);
+    }
+
+    async handleExtensionSubscriptionUpdate(eventName, payload, companyId) {
+        let success = false;
+        let message = "";
+        companyId = Number(companyId);
+        const sellerSubscription = await this.model.getSubscriptionByPlatformId(payload._id, companyId);
+        const existingSubscription = await this.model.getActiveSubscription(companyId);
+        if (!sellerSubscription) {
+            return {
+                success: success,
+                seller_subscription: sellerSubscription,
+                message: `Subscription not found with id ${payload._id}`
+            }
+        }
+    
+        let currentStatus = sellerSubscription.status;
+        sellerSubscription.status = payload.status;
+        
+        if (currentStatus === 'pending' && sellerSubscription.status === 'active' ) {
+            await this.model.activateSubscription(sellerSubscription.id, sellerSubscription.platform_subscription_id);
+            if (existingSubscription) {
+                await this.model.cancelSubscription(existingSubscription.id);
+            }
+            success = true;
+            message = "Subscription activated";
+        } else if (currentStatus === 'pending' && sellerSubscription.status === 'declined') {
+            await this.model.removeSubscription(sellerSubscription.id);
+            success = true;
+            message = "Subscription request is declined by user";
+        } else if (currentStatus === 'active' && sellerSubscription.status === 'cancelled') {
+            await this.model.updateSubscription(sellerSubscription);
+            success = true;
+            message = "Subscription is cancelled by user";
+        }
+        return {
+            success: success,
+            seller_subscription: sellerSubscription,
+            message: message
+        };
+    };
+}
+
+const WebhookHandler = () => new WebhookHandler(SubscriptionModel);
+
+module.exports = {
+    WebhookHandler
+}
